5bae69ac503337af826c6ddb1795fc8b7a12f7ee
ren1023 <renyou.1023@gmail.com>
2024-01-08 11:23:36 +0800

    完成人氣文章彈出視窗功能

--- front/pop.php
+++ front/pop.php
@@ -6,44 +6,64 @@
             <th width="50%">內容</td>
             <th width="">人氣</td>
         </tr>
-        <?php 
-            $total = $News->count(['sh' => 1]);
-            $div = 5;
-            $pages = ceil($total / $div);
-            $now = $_GET['p'] ?? 1;
-            $start = ($now - 1) * $div;
-            // $news = $News->all("limit $start,$div");
-
-            $rows=$News-> all(['sh'=>1]," order by `good` desc  limit $start,$div");//先排序後再取資料
-            foreach($rows as $row){
-            ?>
-        <tr start='<?= $start + 1; ?>'>          
-            <td><?= $row['title'];?></td>
-            <td><?=mb_substr($row['news'],0,25);?>...</td>
-            <td></td>
-        </tr>
         <?php
-            }
+        $total = $News->count(['sh' => 1]);
+        $div = 5;
+        $pages = ceil($total / $div);
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * $div;
+        // $news = $News->all("limit $start,$div");
+
+        $rows = $News->all(['sh' => 1], " order by `good` desc  limit $start,$div"); //先排序後再取資料
+        foreach ($rows as $row) {
+        ?>
+            <tr>
+                <td>
+                <div class='title' data-id="<?=$row['id'];?>" style="cursor:pointer"><?= $row['title']; ?></div>
+                </td>
+                <td>
+                    <div><?= mb_substr($row['news'], 0, 25); ?>...</div>
+                    <div id="p<?=$row['id'];?>" class="pop">
+                        <h3 style="color:lightblue"><?=$row['title'];?></h3>
+                        <pre><?=$row['news'];?></pre>
+                    </div>
+                </td>
+                <td></td>
+            </tr>
+        <?php
+        }
         ?>
     </table>
     <div class="ct">
-		<?php
+        <?php
 
-		if ($now > 1) {
-			$prev = $now - 1;
-			echo " <a href='?do=$do&p=$prev'><</a> ";
-		}
+        if ($now > 1) {
+            $prev = $now - 1;
+            echo " <a href='?do=$do&p=$prev'><</a> ";
+        }
 
-		for ($i = 1; $i <= $pages; $i++) {
-			$fontsize = ($now == $i) ? '24px' : '16px';
-			echo "<a href='?do=$do&p=$i' style='font-size:$fontsize'> $i <a/>";
-		}
+        for ($i = 1; $i <= $pages; $i++) {
+            $fontsize = ($now == $i) ? '24px' : '16px';
+            echo "<a href='?do=$do&p=$i' style='font-size:$fontsize'> $i <a/>";
+        }
 
-		if ($now < $pages) {
-			$next = $now + 1;
-			echo " <a href='?do=$do&p=$next'> > </a> ";
-		}
-		?>
+        if ($now < $pages) {
+            $next = $now + 1;
+            echo " <a href='?do=$do&p=$next'> > </a> ";
+        }
+        ?>
 
-	</div>
+    </div>
 </fieldset>
+<script>
+    $(".title").hover( //當滑鼠滑過標題時
+        function(){
+            $(".pop").hide();//文章內容先是隱藏
+            let id=$(this).data('id');//取得data-id的id=1~n
+            $("#p"+id).show();//#p1~n會顯示
+
+
+        }
+    )
+
+</script>
